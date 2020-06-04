72fdbdc8c11bde57b571920ff0bede80fd45d893
Pedro Paulo B. Suzuki <pedropaulosuzuki1@gmail.com>
2020-06-04 18:40:25 -0300

    Changes on distributions.ts formatting

--- utils/distributions.ts
+++ utils/distributions.ts
@@ -1,22 +1,43 @@
 import bernoulli from './distributions/bernoulli.ts';
-import beta from './distributions/beta.ts'; // not implemented
+import beta from './distributions/beta.ts';
 import binomial from './distributions/binomial.ts';
-import chi_squared from './distributions/chi_squared.ts';  // not implemented
-import exponential from './distributions/exponential.ts';  // not implemented
-import f from './distributions/f.ts';  // not implemented
+import chi_squared from './distributions/chi_squared.ts';
+import exponential from './distributions/exponential.ts';
+import f from './distributions/f.ts';
 import gaussian from './distributions/gaussian.ts';
 import geometric from './distributions/geometric.ts';
-import hypergeometric from './distributions/hypergeometric.ts';  // not implemented
-import kumaraswamy from './distributions/kumaraswamy.ts';  // not implemented
-import logistic from './distributions/logistic.ts';  // not implemented
-import log_logistic from './distributions/log_logistic.ts'; // not implemented
-import log_normal from './distributions/log_normal.ts';  // not implemented
-import negative_binomial from './distributions/negative_binomial.ts';  // not implemented
-import poisson from './distributions/poisson.ts';  // not implemented
-import rayleigh from './distributions/rayleigh.ts'; // not implemented
-import skellam from './distributions/skellam.ts'; // not implemented
-import t from './distributions/t.ts'  // not implemented
+import hypergeometric from './distributions/hypergeometric.ts';
+import kumaraswamy from './distributions/kumaraswamy.ts';
+import logistic from './distributions/logistic.ts';
+import log_logistic from './distributions/log_logistic.ts';
+import log_normal from './distributions/log_normal.ts';
+import negative_binomial from './distributions/negative_binomial.ts';
+import poisson from './distributions/poisson.ts';
+import rayleigh from './distributions/rayleigh.ts';
+import skellam from './distributions/skellam.ts';
+import t from './distributions/t.ts';
 import uniform from './distributions/uniform.ts';
-import weibull from './distributions/weibull.ts'; // not implemented
+import weibull from './distributions/weibull.ts';
 
-export default {bernoulli, beta, binomial, chi_squared, exponential, f, gaussian, geometric, hypergeometric, kumaraswamy, logistic, log_logistic, log_normal, negative_binomial, poisson, rayleigh, skellam, t, uniform, weibull};
+export default {
+    bernoulli,
+    beta,
+    binomial,
+    chi_squared,
+    exponential,
+    f,
+    gaussian,
+    geometric,
+    hypergeometric,
+    kumaraswamy,
+    logistic,
+    log_logistic,
+    log_normal,
+    negative_binomial,
+    poisson,
+    rayleigh,
+    skellam,
+    t,
+    uniform,
+    weibull
+};
