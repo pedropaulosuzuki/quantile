063672f2d682dae8ad4a678fcd6452f3c88e1f5f
Pedro Paulo B. Suzuki <pedropaulosuzuki1@gmail.com>
2020-06-05 18:39:51 -0300

    Adds bernoulli distribution to avaliable distributions.

--- utils/distributions.ts
+++ utils/distributions.ts
@@ -1,26 +1,26 @@
 import bernoulli from './distributions/bernoulli.ts';
-import beta from './distributions/beta.ts';
-import binomial from './distributions/binomial.ts';
-import chi_squared from './distributions/chi_squared.ts';
-import exponential from './distributions/exponential.ts';
-import f from './distributions/f.ts';
-import gaussian from './distributions/gaussian.ts';
-import geometric from './distributions/geometric.ts';
-import hypergeometric from './distributions/hypergeometric.ts';
-import kumaraswamy from './distributions/kumaraswamy.ts';
-import logistic from './distributions/logistic.ts';
-import log_logistic from './distributions/log_logistic.ts';
-import log_normal from './distributions/log_normal.ts';
-import negative_binomial from './distributions/negative_binomial.ts';
-import poisson from './distributions/poisson.ts';
-import rayleigh from './distributions/rayleigh.ts';
-import skellam from './distributions/skellam.ts';
-import t from './distributions/t.ts';
-import uniform from './distributions/uniform.ts';
-import weibull from './distributions/weibull.ts';
+// import beta from './distributions/beta.ts';
+// import binomial from './distributions/binomial.ts';
+// import chi_squared from './distributions/chi_squared.ts';
+// import exponential from './distributions/exponential.ts';
+// import f from './distributions/f.ts';
+// import gaussian from './distributions/gaussian.ts';
+// import geometric from './distributions/geometric.ts';
+// import hypergeometric from './distributions/hypergeometric.ts';
+// import kumaraswamy from './distributions/kumaraswamy.ts';
+// import logistic from './distributions/logistic.ts';
+// import log_logistic from './distributions/log_logistic.ts';
+// import log_normal from './distributions/log_normal.ts';
+// import negative_binomial from './distributions/negative_binomial.ts';
+// import poisson from './distributions/poisson.ts';
+// import rayleigh from './distributions/rayleigh.ts';
+// import skellam from './distributions/skellam.ts';
+// import t from './distributions/t.ts';
+// import uniform from './distributions/uniform.ts';
+// import weibull from './distributions/weibull.ts';
 
 export default {
-    // bernoulli,
+    bernoulli,
     // beta,
     // binomial,
     // chi_squared,
